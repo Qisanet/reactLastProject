cd8a60c46d8ef9c066d7e98134c2012f6ebd784f
Qisanet <ksanetang@gmail.com>
2024-12-18 12:57:53 +0300

    Update Movielist.jsx

--- src/Movielist.jsx
+++ src/Movielist.jsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Search from "./search.jsx";
+import Search from "./Search.jsx";
 import axios from "axios";
 
 export default function Movielist() {
